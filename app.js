18352a60eed50f840a66b5e8b08bc7cafcf620b7
Andrew <andrewbondar2009@gmail.com>
2023-02-07 21:07:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let c = 8;
+let b = 12;
+let p = (c + b) * 2;
+alert(p);
+
+let a = 100;
+let one = a + 10;
+let two = a * 20;
+let three = a - 30;
+let four = a / 5;
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(four);
